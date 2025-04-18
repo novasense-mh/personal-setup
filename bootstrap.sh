ec80b32942a9fcb41c7ef73304c9e276e5d43d05
Marc Hauser <marc.hauser@novasense.ch>
2025-04-18 18:54:39 +0200

    chore: handle sensitive information in vault

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,4 +10,4 @@ pipx install --include-deps ansible
 pipx install ansible-core~=2.17
 sudo python -m pip --break-system-packages install packaging
 ansible-galaxy install -r requirements.yml --force
-ansible-playbook --ask-become-pass main.yml
+ansible-playbook --ask-become-pass --ask-vault-pass -e @secrets.enc main.yml
