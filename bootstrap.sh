da3dfb36b86972bc1855862fad6f03f9fc6d3ecd
Marc Hauser <marc.hauser@novasense.ch>
2025-04-18 17:04:59 -0400

    try-python-on-fresh-osbox

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,10 @@ sudo apt install -y \
   ansible \
   python-is-python3
 
+PATH=$PATH:~/.local/bin/
 python3 -m pip install --user pipx
-pipx install --include-deps ansible
-pipx install ansible-core~=2.17
-sudo python -m pip --break-system-packages install packaging
+python3 -m pipx install --include-deps ansible
+python3 -m pipx install ansible-core~=2.17
+sudo python3 -m pip --break-system-packages install packaging
 ansible-galaxy install -r requirements.yml --force
 ansible-playbook --ask-become-pass --ask-vault-pass -e @secrets.enc main.yml
